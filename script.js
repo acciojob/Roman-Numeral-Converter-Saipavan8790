83980df24350174681569fe911e209602fe83b18
Saipavan8790 <127079433+Saipavan8790@users.noreply.github.com>
2023-10-31 00:22:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// function convertToRoman(num) {
+//   	const obj = {
+//       0:['M',1000], 
+//       1:['D', 500], 
+//       2:['C', 100], 
+//       3:['L', 50], 
+//       4:['X', 10], 
+//       5:['V', 5], 
+//       6:['I', 1]
+//     };
+
+//   //your code here
+// console.log(convertToRoman(36));
+// }
+// // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+
+// // console.log(convertToRoman(36));
+
+
 function convertToRoman(num) {
   	const obj = {
       0:['M',1000], 
@@ -10,14 +29,17 @@ function convertToRoman(num) {
     };
 
   //your code here
-console.log(convertToRoman(36));
+
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+console.log(convertToRoman(36));
 
 
 
 
 // do not edit below this line
 module.exports = convertToRoman
+
+// // do not edit below this line
+// module.exports = convertToRoman
